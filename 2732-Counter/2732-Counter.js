411123f27edcc1f5043f2d88137a586a39fedd2e
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-01-02 12:03:10 +0530

    Solved: 2732

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 3/6/2026, 12:39:58 AM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
