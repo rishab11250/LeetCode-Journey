ef5588242b42eca28c7f09c58c6e0b92dd644325
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-01-06 17:52:36 +0530

    Solved: 792

---
+++ 792-BinarySearch/792-BinarySearch.js
@@ -0,0 +1,22 @@
+// Last updated: 3/6/2026, 12:45:19 AM
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+        else if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+};
