68c2500f8e4e42073ab4a97a332f91bb3de9faf7
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-05 14:58:33 +0530

    Solved: 2820

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 3/6/2026, 12:39:47 AM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
