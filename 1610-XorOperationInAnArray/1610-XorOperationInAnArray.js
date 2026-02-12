5d6cab363805563b3063584afc3a319e91cdf885
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-12 22:59:46 +0530

    Solved: 1610

---
+++ 1610-XorOperationInAnArray/1610-XorOperationInAnArray.js
@@ -0,0 +1,13 @@
+// Last updated: 3/6/2026, 12:41:38 AM
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+var xorOperation = function(n, start) {
+    let result = 0;
+    for(let i = 0; i < n; i++){
+        result ^= start + 2*i;
+    }
+    return result
+};
