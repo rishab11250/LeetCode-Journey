277aa8e6e6f3e2c145eb0eb932f41c90d7e1c6e9
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-12 18:20:26 +0530

    Solved: 4

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.js
@@ -0,0 +1,16 @@
+// Last updated: 3/6/2026, 12:46:32 AM
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+    let arr = [...nums1, ...nums2];
+    arr.sort((a, b) => a - b);
+    if (arr.length % 2 == 0) {
+        return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2;
+    }
+    else {
+        return arr[Math.floor(arr.length / 2)];
+    }
+};
