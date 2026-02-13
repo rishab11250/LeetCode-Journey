0a31da29429ac7e62f5d6a100ee02fa6e35d1ac5
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-13 12:14:48 +0530

    Solved: 2812

---
+++ 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.js
@@ -0,0 +1,9 @@
+// Last updated: 3/6/2026, 12:39:50 AM
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num+(2*t)
+};
