cf5cddea6c6d8cc5b60f93c9181a46b5e1e34f22
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-16 18:11:30 +0530

    Solved: 28

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.js
@@ -0,0 +1,9 @@
+// Last updated: 3/6/2026, 12:46:22 AM
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    return (haystack.indexOf(needle))
+};
