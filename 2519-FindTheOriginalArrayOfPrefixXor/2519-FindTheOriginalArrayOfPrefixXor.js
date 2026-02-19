de7e9f17f8db64cb2a1b052e02a1f3a34893beae
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-19 17:38:34 +0530

    Solved: 2519

---
+++ 2519-FindTheOriginalArrayOfPrefixXor/2519-FindTheOriginalArrayOfPrefixXor.js
@@ -0,0 +1,12 @@
+// Last updated: 3/6/2026, 12:40:19 AM
+/**
+ * @param {number[]} pref
+ * @return {number[]}
+ */
+var findArray = function(pref) {
+    let ans = [pref[0]];
+    for(let i=1;i<pref.length;i++){
+        ans.push(pref[i]^pref[i-1]);
+    }
+    return ans
+};
