60381871e2d7ca5601a3d7f70f1e9c091f45b126
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-20 17:32:30 +0530

    Solved: 3379

---
+++ 3379-ScoreOfAString/3379-ScoreOfAString.js
@@ -0,0 +1,12 @@
+// Last updated: 3/6/2026, 12:39:25 AM
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    let sum = 0;
+    for(let i = 0; i < s.length-1; i++){
+        sum += Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1));
+    }
+    return sum
+};
