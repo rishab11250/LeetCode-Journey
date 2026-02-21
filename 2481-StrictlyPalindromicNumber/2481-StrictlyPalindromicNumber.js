a5c572363b99aef8cfb0659d4c7c70d65dd8c1ad
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-21 15:59:06 +0530

    Solved: 2481

---
+++ 2481-StrictlyPalindromicNumber/2481-StrictlyPalindromicNumber.js
@@ -0,0 +1,14 @@
+// Last updated: 3/6/2026, 12:40:24 AM
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isStrictlyPalindromic = function(n) {
+    // for(let i = 2;i<=(n-2);i++){
+    //     if(n.toString(i).split('').reverse().join('')!==n.toString(i).split('').join('')){
+    //         return false
+    //     }
+    // }
+    // return true
+    return false
+};
