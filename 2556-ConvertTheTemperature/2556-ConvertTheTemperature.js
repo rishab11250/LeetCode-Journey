099ce391255b1d783082fe87b5d1b07f2726717d
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-21 15:53:39 +0530

    Solved: 2556

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.js
@@ -0,0 +1,8 @@
+// Last updated: 3/6/2026, 12:40:16 AM
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    return [celsius + 273.15, (celsius*1.80)+32.00]
+};
