3316133944ad02e0d15b2c848aab7ec2712f4413
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-24 17:06:48 +0530

    Solved: 3918

---
+++ 3918-CheckDivisibilityByDigitSumAndProduct/3918-CheckDivisibilityByDigitSumAndProduct.js
@@ -0,0 +1,16 @@
+// Last updated: 3/6/2026, 12:38:59 AM
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var checkDivisibility = function(n) {
+    let sum = 0;
+    let product = 1;
+    let temp = n
+    while(n>0){
+        sum += n%10;
+        product *= n%10;
+        n = Math.floor(n/10);
+    }
+    return temp%(sum + product) == 0;;
+};
