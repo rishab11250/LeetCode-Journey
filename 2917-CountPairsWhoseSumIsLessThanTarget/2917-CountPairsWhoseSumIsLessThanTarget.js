cd498c4f71a37559ced41a729bfb811e1254ee48
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-24 16:10:28 +0530

    Solved: 2917

---
+++ 2917-CountPairsWhoseSumIsLessThanTarget/2917-CountPairsWhoseSumIsLessThanTarget.js
@@ -0,0 +1,17 @@
+// Last updated: 3/6/2026, 12:39:40 AM
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    let count = 0;
+    for(let i =0; i<nums.length;i++){
+        for(let j = i+1;j<nums.length;j++){
+            if(nums[i] + nums[j] < target){
+                count++
+            }
+        }
+    }
+    return count
+};
