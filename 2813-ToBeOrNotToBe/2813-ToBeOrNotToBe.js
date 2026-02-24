dab2561cbba6a26287e4beef22ec12cda47bff17
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-24 15:46:49 +0530

    Solved: 2813

---
+++ 2813-ToBeOrNotToBe/2813-ToBeOrNotToBe.js
@@ -0,0 +1,22 @@
+// Last updated: 3/6/2026, 12:39:49 AM
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe : function(num){
+            if(val === num) return true;
+            throw new Error ("Not Equal")
+        },
+        notToBe : function(num){
+            if(val !== num) return true;
+            throw new Error ("Equal")
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
