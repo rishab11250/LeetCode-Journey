7c26676baf527c25474cb4a8638d8a450a68d207
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-03-04 19:23:35 +0530

    Solved: 1574

---
+++ 1574-MaximumProductOfTwoElementsInAnArray/1574-MaximumProductOfTwoElementsInAnArray.js
@@ -0,0 +1,14 @@
+// Last updated: 3/6/2026, 12:44:49 AM
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let max = 0;
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            max = Math.max(max, (nums[i]-1)*(nums[j]-1))
+        }
+    }
+    return max
+};
