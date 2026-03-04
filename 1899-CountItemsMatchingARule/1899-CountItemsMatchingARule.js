a86da28514a1aba67300b9cbdf33608e430ccc69
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-03-04 19:21:03 +0530

    Solved: 1899

---
+++ 1899-CountItemsMatchingARule/1899-CountItemsMatchingARule.js
@@ -0,0 +1,28 @@
+// Last updated: 3/6/2026, 12:41:07 AM
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+    let count = 0;
+    for(let i = 0; i < items.length; i++){
+        if(ruleKey == "type"){
+            if(items[i][0] == ruleValue){
+                count++;
+            }
+        }
+        else if(ruleKey == "color"){
+            if(items[i][1] == ruleValue){
+                count++;
+            }
+        }
+        else if(ruleKey == "name"){
+            if(items[i][2] == ruleValue){
+                count++;
+            }
+        }
+    }
+    return count
+};
