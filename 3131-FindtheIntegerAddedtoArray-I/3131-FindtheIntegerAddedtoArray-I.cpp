2bcccaf1549b95e09ea42642c34394c21d92ee99
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-03-07 13:39:13 +0530

    Solved: 3131

---
+++ 3131-FindtheIntegerAddedtoArray-I/3131-FindtheIntegerAddedtoArray-I.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        return nums2[0] - nums1[0];
+    }
+};
