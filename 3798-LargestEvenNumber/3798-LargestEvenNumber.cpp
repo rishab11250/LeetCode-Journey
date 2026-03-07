072f0583c69db382758f61f6467cd0dbab35f75c
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-03-07 16:24:10 +0530

    Solved: 3798

---
+++ 3798-LargestEvenNumber/3798-LargestEvenNumber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string largestEven(string s) {
+    for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '2') {
+                return s.substr(0,i+1);
+        }
+    }
+    return "";
+}
