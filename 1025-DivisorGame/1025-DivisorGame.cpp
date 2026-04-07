003220ee56763beda151a46f5741c8451634ff3a
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-04-07 10:23:29 +0530

    Solved: 1025

---
+++ 1025-DivisorGame/1025-DivisorGame.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool divisorGame(int n) {
+        return n%2==0 ? true : false;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
