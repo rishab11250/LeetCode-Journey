8f78af5be54a4307d7a50d896ee9506998f30550
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-04-10 11:54:53 +0530

    SolvedL 263

---
+++ 263-UglyNumber/263-UglyNumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n <= 0) return false;
+        for(int i = 2;i<=5;i++){
+            while(n%i==0){
+                n/=i;
+            }
+        }
+        return n==1;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
