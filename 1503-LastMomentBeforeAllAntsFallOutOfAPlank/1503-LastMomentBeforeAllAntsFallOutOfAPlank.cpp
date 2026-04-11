18e5f6aa4297431f3d7bcaf8ffb95c2734b91811
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-04-11 14:43:18 +0530

    Solved: 1503

---
+++ 1503-LastMomentBeforeAllAntsFallOutOfAPlank/1503-LastMomentBeforeAllAntsFallOutOfAPlank.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int res = 0;
+        for(int i : left){
+            res = max(res, i);
+        }
+        for(int i : right){
+            res = max(res, n-i);
+        }
+        return res;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
