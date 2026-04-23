b975b2736aaa42cbc78f0b7ec9a708a27eccae7d
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-04-23 09:34:53 +0530

    Solved: 202

---
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        if (n==1 || n==7) return true;
+        else if(n<10) return false;
+        else{
+            int sum = 0;
+            while(n>0){
+                int temp = n%10;
+                sum+= temp*temp;
+                n=n/10;
+            }
+            return isHappy(sum);
+        }
+    }
+};
+
+int main(){
+    
+    return 0;
+}
