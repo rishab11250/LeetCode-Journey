f85ed62696f63167416ce10c7c337040c8ed80aa
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-04-28 23:22:55 +0530

    Solved: 1051

---
+++ 1051-HeightChecker/1051-HeightChecker.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count = 0;
+        vector<int> expected = heights;
+        sort(expected.begin(), expected.end());
+        for(int i = 0 ;i<heights.size();i++){
+            if(heights[i] != expected[i]) count++;
+        }
+        return count;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
