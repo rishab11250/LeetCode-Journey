7b266ee6c4811cf77cc519cb69b380b74e27ff0f
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-04-29 15:01:07 +0530

    Solved: 118

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        for(int i = 0;i<numRows;i++){
+            vector<int> row(i+1,1);
+            for(int j = 1;j<i;j++){
+                row[j] = result[i-1][j-1] + result[i-1][j];
+            }
+            result.push_back(row);
+        }
+        return result;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
