93219d523032b8bf8820808483a5b164da1d6aa2
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-05-06 19:55:24 +0530

    Solved: 292

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n%4;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
