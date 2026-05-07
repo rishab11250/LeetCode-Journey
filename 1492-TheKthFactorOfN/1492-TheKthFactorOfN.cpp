62c722033b58b5b12204ae3ea05635e54c0aced2
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-05-07 20:25:01 +0530

    Solved: 1492

---
+++ 1492-TheKthFactorOfN/1492-TheKthFactorOfN.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int count = 0;
+        for(int i = 1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+            if(count == k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
