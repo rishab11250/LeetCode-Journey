066d7edcd2ba1b3ce5b7fadf43c0ccbbf9b50707
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-06-13 14:53:09 +0530

    Solved: 2000(cpp)

---
+++ 2000-ReversePrefixofWord/cpp/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int index = -1;
+        for(int i = 0;i<word.size();i++){
+            if(word[i] == ch){
+                index = i;
+                break;  
+            }
+        }
+        if(index == -1){
+            return word;
+        }
+        reverse(word.begin(),word.begin()+index+1);
+        return word;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
