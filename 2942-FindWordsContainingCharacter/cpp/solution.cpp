06887acdf8a64877ece6fd77961192064447647e
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-06-13 10:10:38 +0530

    Solved: 2942(cpp)

---
+++ 2942-FindWordsContainingCharacter/cpp/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+        for (int i = 0;i<words.size();i++) {
+            if (words[i].contains(x)) {
+                ans.push_back(i);
+                
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
