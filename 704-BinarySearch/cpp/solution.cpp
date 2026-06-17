ac20b0695129311786478a615d5e8c7efe4b1d3f
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-06-17 09:45:54 +0530

    Solved: 704(cpp)

---
+++ 704-BinarySearch/cpp/solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+            
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+
+int main(){
+    
+    return 0;
+}
