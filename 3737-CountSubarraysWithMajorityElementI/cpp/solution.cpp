a4bd7544e9ffc43ccf6fb69d44043362b60331e0
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-06-25 22:52:37 +0530

    Solved: 3737(cpp)

---
+++ 3737-CountSubarraysWithMajorityElementI/cpp/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        long long ans = 0;
+
+        for (int l = 0; l < n; l++) {
+            int targetCount = 0;
+
+            for (int r = l; r < n; r++) {
+                if (nums[r] == target) {
+                    targetCount++;
+                }
+
+                int len = r - l + 1;
+
+                if (targetCount > len / 2) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
