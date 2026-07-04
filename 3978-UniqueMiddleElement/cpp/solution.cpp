d25e95ae13fe7beb4baf326013976b52daf44691
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-07-05 00:00:17 +0530

    Solved: 3978(cpp)

---
+++ 3978-UniqueMiddleElement/cpp/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int target=nums.size()/2,count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==nums[target])count++;
+            if(count>1)return false;
+        }
+        return true;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
