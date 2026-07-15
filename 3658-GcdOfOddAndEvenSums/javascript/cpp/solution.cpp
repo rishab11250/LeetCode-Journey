79be8e6c7e2fb3f5653ce991da61abbd9a9abeac
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-07-15 10:41:45 +0530

    Solved: 3658(cpp)

---
+++ 3658-GcdOfOddAndEvenSums/javascript/cpp/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
