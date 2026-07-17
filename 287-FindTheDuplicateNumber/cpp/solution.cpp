1b3c31e6b44a4a15640a74179e59f64a9ed91cfe
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-07-17 15:52:05 +0530

    Solved: 287(cpp)

---
+++ 287-FindTheDuplicateNumber/cpp/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i = 1;i<nums.size();i++){
+            if((nums[i]^nums[i-1])==0){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
