4d4fc29d0bdcec3b7a65c2bdb3c05bb2ec6ee192
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-07-20 15:28:19 +0530

    Solved: 237(cpp)

---
+++ 237-DeleteNodeInALinkedList/cpp/solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//   Definition for singly-linked list.
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
