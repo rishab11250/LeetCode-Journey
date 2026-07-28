4bed6556dc61792d4bd1edb83ca3e49a2f51b846
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-07-28 15:06:53 +0530

    Solved: 4000(cpp)

---
+++ 4000-LargestIntegerWithGivenDigitSum/cpp/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int countQuadruplets(vector<int>& nums) {
+        int c = 0, n = nums.size();
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 1; j < n - 2; j++) {
+                for (int k = j + 1; k < n - 1; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        if (nums[i] + nums[j] + nums[k] == nums[l])
+                            c++;
+                    }
+                }
+            }
+        }
+        return c;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
