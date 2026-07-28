57c5d57bfe7127eb468dd260961da2b6ff15d4e5
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-07-28 15:56:22 +0530

    Solved: 1588(cpp)

---
+++ 1588-SumOfAllOddLengthSubarrays/cpp/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int ans = 0;
+        for(int i = 0;i<arr.size();i++){
+            int sum = 0;
+            for(int j = i;j<arr.size();j++){
+                sum += arr[j];
+                if((j-i+1)%2!=0){
+                    ans += sum;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
