3d2da5f1086d5761c2239459949d3cfda80feb30
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-08-01 11:09:42 +0530

    Solved: 177(sql)

---
+++ 177-NthHighestSalary/sql/solution.sql
@@ -0,0 +1,18 @@
+CREATE FUNCTION getNthHighestSalary (N INT) RETURNS INT BEGIN
+set
+    n = n -1;
+
+RETURN (
+    SELECT DISTINCT
+        salary
+    FROM
+        Employee
+    ORDER BY
+        salary DESC
+    LIMIT
+        1
+    OFFSET
+        N
+);
+
+END
