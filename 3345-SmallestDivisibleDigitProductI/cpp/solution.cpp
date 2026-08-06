44e673e2992451ca9d7500b1b74c429e8100d753
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-08-06 10:21:13 +0530

    Solved: 3345(cpp)

---
+++ 3345-SmallestDivisibleDigitProductI/cpp/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        int ans = n;
+        while(1){
+            int prod = 1;
+            int x = ans;
+            while(x>0){
+                prod *= x%10;
+                x = x/10;
+            }
+            if(prod%t == 0) break;
+            ans++;
+        }
+        return ans;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
