f9d5f89e3a8f7bd689d408835d770d68b768de9b
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-08-09 23:19:39 +0530

    Solved: 4014(cpp)

---
+++ 4014-MinimumTotalPriceAfterApplyingDiscounts/cpp/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    double minPrice(vector<int>& prices, vector<int>& discounts) {
+        int n = prices.size() ;
+        int m = discounts.size() ;
+        sort( prices.rbegin() , prices.rend() ) ;
+        sort( discounts.rbegin() , discounts.rend() ) ;
+        double ans = 0;
+        int i = 0;
+        int j = 0;
+        while ( i < n && j < m ) {
+            ans += (double)( prices[i] * (double)(100-discounts[j]) / 100 ) ;
+            i++ ;
+            j++ ;
+            
+        }
+        while ( i < n ) ans += prices[i++] ;
+        return ans ;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
