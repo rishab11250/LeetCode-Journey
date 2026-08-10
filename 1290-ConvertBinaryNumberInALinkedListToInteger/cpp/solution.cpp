c5ae9046f579bdcb0700570af325108be6b26c91
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-08-10 14:50:35 +0530

    Solved: 1290(cpp)

---
+++ 1290-ConvertBinaryNumberInALinkedListToInteger/cpp/solution.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        string store;
+        while(head!=nullptr){
+            store += to_string(head->val);
+            head = head->next;
+        }
+        return stoi(store,nullptr,2);
+    }
+};
+
+int main(){
+    
+    return 0;
+}
