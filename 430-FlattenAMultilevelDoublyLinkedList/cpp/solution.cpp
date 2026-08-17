67bc2da3ba6951286e8be719db8b61c83ee62f27
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-08-17 15:40:24 +0530

    Solved: 430(cpp)

---
+++ 430-FlattenAMultilevelDoublyLinkedList/cpp/solution.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+
+public:
+    Node* flatten(Node* head) {
+        Node* temp = head;
+        Node* tempS = head;
+        stack <Node*> st;
+
+        while(temp != nullptr){
+            if(temp -> child != nullptr) {
+                if(temp->next != nullptr)st.push(temp->next);
+                temp -> next = temp->child;
+                temp -> child = nullptr;
+                temp -> next -> prev = temp;
+            }
+            tempS = temp;
+            temp = temp -> next;
+        }
+        while(!st.empty()){
+            temp = st.top();
+            st.pop();
+            tempS -> next = temp;
+            temp-> prev = tempS;
+            while(tempS->next != nullptr) tempS = tempS -> next; 
+        }
+        return head;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
