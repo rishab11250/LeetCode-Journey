19a0c79a9ceb12e2b33b8d85eb2a84d7ddf743be
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-09-02 17:12:42 +0530

    Solved: 3875(cpp)

---
+++ 3875-ConstructUniformParityArrayI/cpp/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
