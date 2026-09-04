0240c912a01b1e45f41f9a9a52fca7eabcdc7b97
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-09-04 12:00:31 +0530

    Solved: 1(cpp) using map

--- 1-TwoSum/cpp/solution.cpp
+++ 1-TwoSum/cpp/solution.cpp
@@ -1,13 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
         for(int i = 0;i<nums.size();i++){
-            for(int j = i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
+            int diff = target - nums[i];
+            if(mp.find(diff) != mp.end()){
+                return {mp[diff], i};
             }
+            mp[nums[i]] = i;
         }
         return {};
     }
 };
+
+int main(){
+    
+    return 0;
+}
