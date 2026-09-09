8f04e05bcb5bda2231d812eab4ed14c37b01a2a7
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-09-09 11:14:26 +0530

    Solved: 234(cpp) with 2 pointer approch

--- 234-PalindromeLinkedList/cpp/solution.cpp
+++ 234-PalindromeLinkedList/cpp/solution.cpp
@@ -14,26 +14,65 @@ using namespace std;
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ // Solution 1 Space O(n) and time O(n)
+ 
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         if(head == NULL || head->next == NULL){
+//             return (head);
+//         }
+//         ListNode *r_head = nullptr;
+//         ListNode *curr = head;
+//         while(curr!=nullptr){
+//             ListNode *temp = new ListNode(curr->val);
+//             temp->next = r_head;
+//             r_head = temp;
+//             curr = curr->next;
+//         }
+//         while(head && r_head){
+//             if(head->val != r_head->val){
+//                 return false;
+//             }
+//             head = head->next;
+//             r_head = r_head->next;
+//         }
+//         return true;
+//     }
+// };
+
+
+// Solution 2 Space O(1) and time O(n)
+
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         if(head == NULL || head->next == NULL){
             return (head);
         }
-        ListNode *r_head = nullptr;
-        ListNode *curr = head;
-        while(curr!=nullptr){
-            ListNode *temp = new ListNode(curr->val);
-            temp->next = r_head;
-            r_head = temp;
-            curr = curr->next;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        while(head && r_head){
-            if(head->val != r_head->val){
+        ListNode* r = fast == nullptr ? slow : slow->next;
+        ListNode* prev= nullptr;
+        while(r){
+            ListNode* temp = r->next;
+            r->next = prev;
+            prev= r;
+            r = temp;
+        }
+        slow = prev;
+        while(head && slow){
+            if(head->val != slow->val){
                 return false;
             }
             head = head->next;
-            r_head = r_head->next;
+            slow = slow->next;
         }
         return true;
     }
